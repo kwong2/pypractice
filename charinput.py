b209ffc82e217a458a3e91feb4703d89e58ffe64
Kevin Wong <k.wong4@yahoo.com>
2017-08-07 16:35:15 -0400

    future added

--- charinput.py
+++ charinput.py
@@ -6,4 +6,10 @@ age = int(age)
 
 print ("Your name is " + str(name) + " and you are " + str(age) + " years old")
 years = int(100 - age)
+"\n"
 print ("You will turn 100 in " + str(years) + " years.")
+
+"\n"
+"\n"
+future = (2016 + years)
+print (str(years) + " years from now is " + str(future))
