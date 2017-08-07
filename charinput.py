8479c765de868fa1dc10ddd977379192324c9afc
Kevin Wong <k.wong4@yahoo.com>
2017-08-07 16:09:35 -0400

    fixed data types

--- charinput.py
+++ charinput.py
@@ -1,9 +1,9 @@
 name = input("What is your name?")
-print ("Hello " + name)
+print ("Hello " + str(name))
 
 age = input("How old are you?")
 age = int(age)
 
-print ("Your name is " + name + " and you are " + age + " old")
-years = (100 - age)
-print ("You will turn 100 in " + years + " years.")
+print ("Your name is " + str(name) + " and you are " + str(age) + " years old")
+years = int(100 - age)
+print ("You will turn 100 in " + str(years) + " years.")
