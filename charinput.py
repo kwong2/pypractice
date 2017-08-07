d8bbe9c25a91d1a68be4bf029506f15812e0d354
Kevin Wong <k.wong4@yahoo.com>
2017-08-07 16:02:35 -0400

    first commit

---
+++ charinput.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+print ("Hello " + name)
+
+age = input("How old are you?")
+age = int(age)
+
+print ("Your name is " + name + " and you are " + age + " old")
+years = (100 - age)
+print ("You will turn 100 in " + years + " years.")
