6f752acad02b0129e59eb5721aa79ead549aed83
Kevin Wong <k.wong4@yahoo.com>
2017-08-10 11:37:58 -0400

    solved basic

--- evenodd.py
+++ evenodd.py
@@ -1,2 +1,6 @@
 number = input("By giving me a number, I will tell you if it is even or odd."
 )
+if number % 2 == 0:
+	print("this is an EVEN number")
+else:
+	print("this number is ODD, like you.")
