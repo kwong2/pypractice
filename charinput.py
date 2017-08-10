587522e12b2d1e9ddaa7ac0ed3d9c0917cf59bf6
Kevin Wong <k.wong4@yahoo.com>
2017-08-10 11:34:45 -0400

    new problem

--- charinput.py
+++ charinput.py
@@ -1,3 +1,5 @@
+# http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
 # Exercise 1
 
 # Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
---
+++ evenodd.py
@@ -0,0 +1,2 @@
+number = input("By giving me a number, I will tell you if it is even or odd."
+)
