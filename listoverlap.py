5d73247e3f10414d1aa3c45a5e0cf38d2d2ad94b
Kevin Wong <k.wong4@yahoo.com>
2017-08-11 15:33:16 -0400

    solved initial overlap

---
+++ listoverlap.py
@@ -0,0 +1,12 @@
+# create a method that takes two arrays and returns the common elements in a new arrays
+
+
+a=[2,3,4,5,6,7,8,9]
+b=[1,3,5,7,9,10,11,12]
+
+for num in a:
+	if num in b:
+		print num	
+
+
+		
