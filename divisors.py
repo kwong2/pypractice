076298445c068487371087e1908caf233a0d3c61
Kevin Wong <k.wong4@yahoo.com>
2017-08-11 18:14:19 -0400

    link added in comments

--- divisors.py
+++ divisors.py
@@ -1,10 +1,11 @@
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+# http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 
 number = input("What number do you want the divisors for ")
 
 for num in range (number, 101):
-	if num % number == 0 :
-		print(num)
+	if num % number == 0:
+		return num 
 
 
 
