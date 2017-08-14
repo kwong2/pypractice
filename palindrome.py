25a48486727bffb76a8b7d276a68fd2857c41a59
Kevin Wong <k.wong4@yahoo.com>
2017-08-14 16:30:54 -0400

    link added in comments

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,5 @@
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.
+# http://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 wrd = input("So what word should we check if it is a palindrome?")
 wrd_reverse = wrd[::-1]
 
